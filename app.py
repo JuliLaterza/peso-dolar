fc8345d1389c83a89c63baac24050acdb1d750eb
jlaterza <laterzajulian@gmail.com>
2021-12-05 10:30:45 -0300

    v1.0

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ from tkinter import *
 #Windows
 root = Tk()
 root.title('Conversor')
-root.geometry('150x200')
 
-def calcular():
-    pass
-
+def calcular(*args):
+    try:
+        value = float(dolar.get())
+        p = float(value*200) 
+        peso.set(p) #Le seteo el valor de P a pesos como variable y luego la muestro.
+    except ValueError:
+        peso.set('ERROR')
 
 #Box
 frame = Frame(root,padx=15,pady=15)
 frame.grid(row=0,column=0)
 
-dolar = StringVar()
+dolar = StringVar() #We have 
 dolar_input = Entry(frame,width=7, textvariable=dolar)
 dolar_input.grid(column=1,row=0)
 
@@ -22,9 +25,12 @@ peso.set('24242')
 Label(frame, textvariable=peso).grid(column=1,row=1)
 
 
-Button(frame,textvariable='Calcular',command=calcular).grid(column=2,row=2)
-Label(frame,text='dolares').grid(column=0,row=0)
+Button(frame,text='Calculate',command=calcular).grid(column=2,row=0,padx=10,pady=10)
+Label(frame,text='Dolars').grid(column=0,row=0)
 Label(frame,text='es igual a').grid(column=0,row=1)
+Label(frame,text='pesos argentinos').grid(column=3,row=1)
 
+dolar_input.focus() #Focusear el Inputs
+root.bind("<Return>", calcular)
 
 root.mainloop()
